752f8f1167c390114b721eb80abdb935a4ba4d13
Ikem Ikenna <97095222+Ikesee-Ikemefuna@users.noreply.github.com>
2024-01-26 20:17:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_file
 
 import json
 
 app = Flask(__name__)
 
-# Load the data from Diabetes.json
+# Load the data from fhir_data.json
 with open('fhir_data.json') as f:
     data = json.load(f)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    # Route for serving styles.css
+
+# Route for serving styles.css
 @app.route('/styles.css')
 def get_styles():
     return send_file('static/styles.css')
@@ -20,10 +21,12 @@ def get_styles():
 @app.route('/script.js')
 def get_script():
     return send_file('static/script.js')
-    # Route for serving script.js
+
+# Route for serving fhir_data.json
 @app.route('/fhir_data.json')
 def get_fhir_data():
     return send_file('fhir_data.json')
+
 @app.route('/get_data/<search_term>')
 def get_filtered_data(search_term):
     if search_term.lower() == 'all':
@@ -31,13 +34,8 @@ def get_filtered_data(search_term):
         return jsonify(data)
     else:
         # Filter the dataset for the specific name
-        filtered_data = [entry for entry in data if entry['Name'].lower() == search_term.lower()]
+        filtered_data = [entry for entry in data if entry.get('Name', '').lower() == search_term.lower()]
         return jsonify(filtered_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-                                                                                                                                                                       from flask import Flask, render_template, jsonifyimport json
-
